08fd0afdee9b483cf91570c4f0e974ac20d583dd
Yoav Zelinger <yoavzelinger11@gmail.com>
2026-01-18 23:20:37 +0200

    creating a forest builder

---
+++ Tester/Builders/RandomForestClassifierBuilder.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import numpy as np
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import accuracy_score
+
+import Tester.TesterConstants as tester_constants
+
+def build_forest(
+        X_train: pd.DataFrame,
+        y_train: pd.Series,
+        model: RandomForestClassifier = None
+        ) -> RandomForestClassifier:
+    """
+    Build a random forest classifier based on the given data and features.
+
+    Parameters:
+        X_train (DataFrame): The training features set.
+        y_train (Series): The training labels.
+        model (RandomForestClassifier, optional): An initialized model to train on (with previous best-chosen hyperparameters).
+
+        If validation data not provided then it is taken from as 0.2 from the training data.
+
+    Returns:
+        RandomForestClassifier: The random forest classifier.
+    """
+    np.random.seed(tester_constants.RANDOM_STATE)
+    
+    if model is not None:
+        model.fit(X_train, y_train)
+        return model
+    
+    X_train, X_validation, y_train, y_validation = train_test_split(X_train, y_train, test_size=tester_constants.VALIDATION_SIZE, random_state=tester_constants.RANDOM_STATE)
+
+    # Grid search modification
+    modified_X_train, modified_y_train = X_train, y_train
+    classes_counts = y_train.value_counts()
+    if classes_counts.min() == 1:
+        # Duplicate the rows with that one instance
+        min_classes = classes_counts[classes_counts == 1].index
+        for class_name in min_classes:
+            sample_filter = (modified_y_train == class_name)
+            modified_X_train = pd.concat([modified_X_train, modified_X_train[sample_filter]], ignore_index=True)
+            modified_y_train = pd.concat([modified_y_train, pd.Series([class_name])], ignore_index=True)
+    cross_validation_split_count = min(tester_constants.CROSS_VALIDATION_SPLIT_COUNT , modified_y_train.value_counts().min())
+
+    random_forest_classifier = RandomForestClassifier(random_state=tester_constants.RANDOM_STATE)
+    # Find best parameters using grid search cross validation (on training data)
+    grid_search_classifier = GridSearchCV(estimator=random_forest_classifier, 
+                                     param_grid=tester_constants.FOREST_PARAM_GRID, 
+                                     cv=cross_validation_split_count)
+    grid_search_classifier.fit(modified_X_train, modified_y_train)
+    grid_search_best_params = grid_search_classifier.best_params_ # Hyperparameters
+    random_forest_classifier = RandomForestClassifier(criterion=grid_search_best_params["criterion"],
+                                                      random_state=tester_constants.RANDOM_STATE
+                                                      )
+    
+    model = RandomForestClassifier(criterion=grid_search_best_params["criterion"], 
+                                   random_state=tester_constants.RANDOM_STATE
+                                   )
+    model.fit(X_train, y_train)
+    model.best_accuracy = accuracy_score(y_validation, model.predict(X_validation))
+
+    return model
