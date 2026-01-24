765550a9fd57363d8f010fe85109bc25c4c04a0c
Yoav Zelinger <yoavzelinger11@gmail.com>
2026-01-24 19:00:47 +0200

    add diagnosers

--- Tester/TesterConstants.py
+++ Tester/TesterConstants.py
@@ -44,7 +44,7 @@ TREE_PARAM_GRID = {
     "criterion": _CRITERIONS,
     "max_leaf_nodes": _TREE_MAX_LEAF_NODES
 }
-_FOREST_NUM_ESTIMATORS = 10
+_FOREST_NUM_ESTIMATORS = 100
 FOREST_PARAM_GRID = {
     "criterion": _CRITERIONS,
     "n_estimators": [_FOREST_NUM_ESTIMATORS]
